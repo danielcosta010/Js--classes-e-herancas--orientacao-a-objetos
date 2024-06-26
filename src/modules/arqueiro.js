060745b3e2cfcd240f35acffb51b5bd19d015a47
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-06-26 01:08:06 -0300

    Update arqueiro.js

--- src/modules/arqueiro.js
+++ src/modules/arqueiro.js
@@ -2,18 +2,21 @@ import { Personagem } from "./personagem.js";
 
 export class Arqueiro extends Personagem {
   
-  tipo = 'Arqueiro'
   destreza
+  static tipo = 'Arqueiro'
+  static descricao = 'Porta um arco ágil e imprecionante precisão'
 
-  constructor(nome, level, destreza){
-    super(nome, level)
+  constructor(nome, destreza){
+    super(nome)
     this.destreza = destreza
   }
 
   obterInsignia() {
-    if(this.destreza >= 5) {
+    if(this.destreza >= 5 && this.level >= 6) {
       return 'Dominador de flechas'
     }
     return super.obterInsignia()
   }
 }
+
+
