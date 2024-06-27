cd6794dc624ac5628f525d63ae727f534f8b3ca8
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-06-27 14:12:43 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import { Guerreiro } from "./modules/guerreiro.js";
 const magoAntonio = new Mago('Antônio', 4, 'Água', 4, 8)
 const magaClara = new Mago('Clara', 9, 'Fogo', 6, 8)
 
-const aqueiroBruno = new Arqueiro('Bruno', 4, 8)
+const aqueiroBruno = new Arqueiro('Bruno', 6, 8)
 
-const aqueiroMagoAdão = new ArqueiroMago('Adão', 5, 7, 'Flecha e Fogo', 4, 8)
+const aqueiroMagoAdão = new ArqueiroMago('Adão', 7, 7, 'Flecha e Fogo', 7, 8)
 
-const guerrerioLeandro = new Guerreiro('Leandro', 8, 5)
+const guerrerioLeandro = new Guerreiro('Leandro', 8)
 
 const personagens = [magoAntonio, magaClara, aqueiroBruno, aqueiroMagoAdão, guerrerioLeandro]
 
